f69ed1ddb45ab8c86d37f6d5aafb1a5221b8845d
Amrut <amrutpanda7789@gmail.com>
2020-06-17 19:20:39 +0530

    ADDED:AddressBookUtil for read input from user

--- src/main/java/com/bridgelabz/addressbook/utils/AddressBookUtil.java
+++ src/main/java/com/bridgelabz/addressbook/utils/AddressBookUtil.java
@@ -1,4 +1,23 @@
 package com.bridgelabz.addressbook.utils;
+import java.util.Scanner;
 
+/**
+ * @Authpr:Amrut
+ * this is the utility class where we use scanner object and use scanner object every class for input
+ */
 public class AddressBookUtil {
+    private final static Scanner scanner = new Scanner(System.in);
+
+    /*define default constructor and make it as private so that object cannot be created outside the class*/
+    private AddressBookUtil(){
+
+    }
+    /*function to get number input from user*/
+    public static int getUserNumber(){
+        return scanner.nextInt();
+    }
+    /*function to get string input from user*/
+    public static String getUserString(){
+        return scanner.nextLine();
+    }
 }
