81b755a89fb328458cf3cb45439fd98a058e97bc
Amrut <amrutpanda7789@gmail.com>
2020-06-17 17:10:43 +0530

    ADDED:Welcome to AddressBook Problem

---
+++ src/main/java/com/bridgelabz/addressbook/main/AddressBookMain.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.addressbook.main;
+
+/**
+ *This is the main class contains main method and handles everyother packages and classes.
+ *@author:Amrut
+ */
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to addressbook problem");
+    }
+}
