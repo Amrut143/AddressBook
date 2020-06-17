00841c4e47d61b73c4acb947cd503befa0c93fe0
Amrut <amrutpanda7789@gmail.com>
2020-06-17 20:16:28 +0530

    ADDED:AddressBook edit method implementation

--- src/main/java/com/bridgelabz/addressbook/services/AddressBookServiceImpl.java
+++ src/main/java/com/bridgelabz/addressbook/services/AddressBookServiceImpl.java
@@ -3,6 +3,8 @@ import com.bridgelabz.addressbook.models.Person;
 import com.bridgelabz.addressbook.utils.AddressBookRepo;
 import com.bridgelabz.addressbook.utils.AddressBookUtil;
 
+import java.util.List;
+
 /**
  *Creating implementation class for AddressBookService interface for method implementation
  *@author:Amrut
@@ -44,4 +46,62 @@ public class AddressBookServiceImpl implements IAddressBookService {
         repo.addToAddressBook(person);
     }
 
+    @Override
+    public void editPerson() {
+        Person p = getPersonName();
+        if(p != null ){
+            System.out.print("Enter Address: ");
+            final String address = AddressBookUtil.getUserString();
+            p.setAddress(address);
+
+            System.out.print("Enter City: ");
+            final String city =  AddressBookUtil.getUserString();
+            p.setCity(city);
+
+            System.out.print("Enter State: ");
+            final String state = AddressBookUtil.getUserString();
+            p.setState(state);
+
+            System.out.print("Enter Zip code: ");
+            final String zipCode = AddressBookUtil.getUserString();
+            p.setZipCode(zipCode);
+
+            System.out.print("Enter Phone number: ");
+            final String phone = AddressBookUtil.getUserString();
+            p.setPhone(phone);
+            saveChange();
+
+        }else{
+            System.out.println("data not found.");
+        }
+    }
+    @Override
+    public void display(){
+        repo.displayContact();
+    }
+    /*override method to save the changes made*/
+    @Override
+    public void saveChange() {
+        List<Person> addressBook = repo.addressBook;
+    }
+    private Person getPersonName(){
+        AddressBookUtil.getUserString();
+        display();
+        System.out.print("Enter first name of person you want to edit: ");
+        String firstName = AddressBookUtil.getUserString();
+        Person p = findPerson(firstName);
+        return p;
+    }
+
+    @Override
+    public Person findPerson(String firstName){
+        Person returnPerson = null;
+        for(Person p : repo.addressBook){
+            if(firstName.equals(p.getFirstName())){
+                returnPerson = p;
+                break;
+            }
+        }
+        return returnPerson;
+    }
 }
