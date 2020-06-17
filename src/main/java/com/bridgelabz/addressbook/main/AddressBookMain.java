e05d36eb743af2bb59c777880d4a91aea5474ff6
Amrut <amrutpanda7789@gmail.com>
2020-06-17 19:22:31 +0530

    ADDED:Person added to addressbook

--- src/main/java/com/bridgelabz/addressbook/main/AddressBookMain.java
+++ src/main/java/com/bridgelabz/addressbook/main/AddressBookMain.java
@@ -1,4 +1,8 @@
 package com.bridgelabz.addressbook.main;
+import com.bridgelabz.addressbook.utils.AddressBookRepo;
+import com.bridgelabz.addressbook.utils.AddressBookUtil;
+import com.bridgelabz.addressbook.services.IAddressBookService;
+import com.bridgelabz.addressbook.services.AddressBookServiceImpl;
 
 /**
  *This is the main class contains main method and handles everyother packages and classes.
@@ -6,6 +10,12 @@ package com.bridgelabz.addressbook.main;
  */
 public class AddressBookMain {
     public static void main(String[] args) {
+
+        /*creating the implementation class object*/
+        final IAddressBookService iAddressBookService = new AddressBookServiceImpl(new AddressBookRepo());
         System.out.println("Welcome to addressbook problem");
+
+        iAddressBookService.addPerson();
+        System.out.println("Contact Added::");
     }
 }
